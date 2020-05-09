5506cf36dc12fc94972565c93b2231036aaa8de9
jaycohen88 <jaycohen88@gmail.com>
2020-05-08 23:17:18 -0400

    make numbers look nicer

--- MLS_Salary_Shiny_App/global.R
+++ MLS_Salary_Shiny_App/global.R
@@ -7,3 +7,5 @@ salaries = fread(file =
 "/Users/jaycohen/Documents/NYCDSA/Shiny_Project/MLS_Salary_Shiny_Project/Final_Salary_Data.csv")
 
 
+salaries$current_annualized_base_salary=formatC(salaries$current_annualized_base_salary,digits=0,format='f',big.mark=',')
+salaries$annualized_average_guaranteed_comp=formatC(salaries$annualized_average_guaranteed_comp,digits=0,format='f',big.mark=',')
--- MLS_Salary_Shiny_App/server.R
+++ MLS_Salary_Shiny_App/server.R
@@ -1,12 +1,11 @@
 function(input, output) {
-    output$salarytable = renderDT(
-        salaries %>%
-            select('Season'=season,'Club'=club_name,'Last Name'=last_name, 'First Name'=first_name,'Position'=position,
-            'Annualized Base Salary'=current_annualized_base_salary,
-            'Annualized Average Guaranteed Compensation'=annualized_average_guaranteed_comp)
-            %>% filter((Club == input$club) & 
-                     (Season >= input$season[1] & Season <= input$season[2]))
-            %>% filter(str_detect(Position, paste(input$position, collapse='|'))),
-        rownames=FALSE)
+  output$salarytable = renderDT(
+    salaries %>%
+      select('Season'=season,'Club'=club_name,'Last Name'=last_name, 'First Name'=first_name,'Position'=position,
+             'Annualized Base Salary ($)'=current_annualized_base_salary,
+             'Annualized Average Guaranteed Compensation ($)'=annualized_average_guaranteed_comp)
+    %>% filter((Club == input$club) & 
+                 (Season >= input$season[1] & Season <= input$season[2]))
+    %>% filter(str_detect(Position, paste(input$position, collapse='|'))),
+    rownames=FALSE)
 }
-
