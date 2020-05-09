cb940803733a90a7a39cd5d53026536afb5f93d7
jaycohen88 <jaycohen88@gmail.com>
2020-05-09 12:42:02 -0400

    inputs moved to datatable tab

--- MLS_Salary_Shiny_App/ui.R
+++ MLS_Salary_Shiny_App/ui.R
@@ -1,37 +1,49 @@
 dashboardPage(
+    
     dashboardHeader(title = 'MLS Salary Data 2007-2019',
                     titleWidth = 300),
+    
     dashboardSidebar(
         width = 300,
         sidebarUserPanel('By: Jay Cohen'),
         sidebarMenu(
-            menuItem("Data", tabName = "data", icon = icon("database"))),
-        
-        selectizeInput(inputId = "club",
-                       label = "Club",
-                       choices = unique(salaries$club_name),
-                       selected = 'Atlanta United',
-                       multiple = TRUE),
-        
-        sliderInput(inputId = "season",
-                    label = "Season",
-                    min = 2007, 
-                    max = 2019, 
-                    value = c(2007,2019),
-                    step = 1,
-                    sep = ""),
-        
-        checkboxGroupInput(inputId = "position",
-                           label = "Position", 
-                           choices = c('GK','D','M','F','None Listed'), 
-                           selected = c('GK','D','M','F','None Listed'))   
+            menuItem("Data", tabName = "data", icon = icon("database")))
     ),
     
     dashboardBody(
         tabItems(
             tabItem(tabName = "data",
-                    fluidRow(box(DT::DTOutput("salarytable"),
-                                 width = 14)))
-        )
-    )
+                    fluidRow(
+                        box(width = 4,
+                            selectizeInput(inputId = "club",
+                                           label = "Club",
+                                           choices = unique(salaries$club_name),
+                                           selected = 'Atlanta United',
+                                           multiple = TRUE)),
+                            
+                        box(width = 4,  
+                            sliderInput(inputId = "season",
+                                        label = "Season",
+                                        min = 2007, 
+                                        max = 2019, 
+                                        value = c(2007,2019),
+                                        step = 1,
+                                        sep = "")),
+                            
+                        box(width = 4,
+                            checkboxGroupInput(inputId = "position",
+                                               label = "Position", 
+                                               choices = c('GK','D','M','F','None Listed'), 
+                                               selected = c('GK','D','M','F','None Listed'),
+                                               inline = TRUE))
+                        ),
+                    fluidRow(
+                        box(
+                            DT::DTOutput("salarytable"),
+                                 width = 12)),
+            
+                    
+                    )
+                )
+            )
 )
